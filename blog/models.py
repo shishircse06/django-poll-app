150f598e55da9ceb732945ff4d7a30094859586a
Shishir <shishir_ruet@yahoo.com>
2016-07-25 00:33:06 +0600

    blog list view added

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
 
 # Create your models here.
@@ -22,8 +23,13 @@ class Post(models.Model):
 
     class Meta:
     	ordering = ('-publish',)
+ 
     def __str__(self):
     	return self.title
+ 
+    def get_absolute_url(self):
+    	return reverse('blog:post_detail', args=[self.publish.year,self.publish.strftime('%m'),self.publish.strftime('%d'),self.slug])	
+
     objects = models.Manager() # The default manager.
     published = PublishedManager() # Our custom manager.
 
