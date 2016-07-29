27e560d316e0bc86d819265e27c5d2d4a7347681
Shishir <shishir_ruet@yahoo.com>
2016-07-29 17:11:08 +0600

    find similar post

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from .models import Post, Comment
 from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
 from taggit.models import Tag
+from django.db.models import Count
 
 class PostListView(ListView):
 	queryset = Post.published.all()
@@ -61,10 +62,16 @@ def post_detail(request, year, month, day, post):
         comment_form = CommentForm()
 
     # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags',
+                                                                             '-publish')[:4]
+
    
     return  render(request, 'blog/post/detail.html', {'post': post,
                                                      'comments': comments,
-                                                     'comment_form': comment_form})
+                                                     'comment_form': comment_form,
+                                                     'similar_posts': similar_posts})
 
 
 
