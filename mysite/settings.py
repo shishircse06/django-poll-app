84f5a6f28c2ac722078ef1b7c17073cc420f963f
Shishir <shishir_ruet@yahoo.com>
2016-07-31 21:16:16 +0600

    create sitemaps

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+SITE_ID = 1
+
 
 # Application definition
 
@@ -40,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
 ]
 
 MIDDLEWARE_CLASSES = [
--- mysite/urls.py
+++ mysite/urls.py
@@ -15,9 +15,17 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.sitemaps.views import sitemap 
+from blog.sitemaps import PostSitemap
+
+sitemaps = {
+       'posts': PostSitemap,
+}
+
 
 urlpatterns = [
 	url(r'^blog/', include('blog.urls', namespace='blog', app_name='blog')),
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps},name='django.contrib.sitemaps.views.sitemap'),
 ]
